01121bca186deab6a5790d17e1a2a7aa4abb7ebc
Aurel Medvegy <medvegy@turnonline.biz>
2018-04-09 06:05:28 +0200

    AppEngineEnvironmentContextListener guice listener has employed.

--- src/main/java/biz/turnonline/ecosystem/origin/frontend/GuiceServletConfig.java
+++ src/main/java/biz/turnonline/ecosystem/origin/frontend/GuiceServletConfig.java
@@ -1,41 +1,28 @@
 package biz.turnonline.ecosystem.origin.frontend;
 
-import com.google.appengine.api.utils.SystemProperty;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Stage;
-import com.google.inject.servlet.GuiceServletContextListener;
+import org.ctoolkit.services.guice.AppEngineEnvironmentContextListener;
 
 /**
  * @author <a href="mailto:medvegy@turnonline.biz">Aurel Medvegy</a>
  */
 public class GuiceServletConfig
-        extends GuiceServletContextListener
+        extends AppEngineEnvironmentContextListener
 {
-    private Injector getTestInjector()
+    @Override
+    protected Injector getDevelopmentInjector()
     {
         return Guice.createInjector( new FrontendModule(),
                 new FrontendServletModule() );
     }
 
-    private Injector getProductionInjector()
+    @Override
+    protected Injector getProductionInjector()
     {
         return Guice.createInjector( Stage.PRODUCTION,
                 new FrontendModule(),
                 new FrontendServletModule() );
-    }
-
-    @Override
-    protected final Injector getInjector()
-    {
-        if ( SystemProperty.environment.value() == SystemProperty.Environment.Value.Production )
-        {
-            // The app is running on App Engine...
-            return getProductionInjector();
-        }
-        else
-        {
-            return getTestInjector();
-        }
     }
 }
