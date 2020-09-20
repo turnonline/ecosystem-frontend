73257ad42ba694130324813c5f4ac152302c37b3
Aurel Medvegy <medvegy@turnonline.biz>
2020-09-20 09:47:53 +0200

    ModelFactory package fixed.

--- src/main/java/biz/turnonline/ecosystem/origin/frontend/model/ModelFactory.java
+++ src/main/java/biz/turnonline/ecosystem/origin/frontend/model/ModelFactory.java
@@ -1,23 +1,5 @@
-/*
- * Copyright 2020 Comvai, s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package biz.turnonline.ecosystem.origin.frontend.model;
 
-import biz.turnonline.ecosystem.origin.frontend.model.ControllerModel;
-import biz.turnonline.ecosystem.origin.frontend.model.FirebaseConfig;
-import biz.turnonline.ecosystem.origin.frontend.model.GwtConfig;
-import biz.turnonline.ecosystem.origin.frontend.model.Messages;
 import com.google.common.base.Strings;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Prototype;
