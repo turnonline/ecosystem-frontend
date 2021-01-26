a7c127e581a371adc08c714fab91762210e07dfe
Aurel Medvegy <medvegy@turnonline.biz>
2021-01-26 22:35:07 +0100

    Error executing FreeMarker template. Only supported language locale is allowed to be set by cookie, otherwise default locale will be used. #TECO-218

--- src/main/java/biz/turnonline/ecosystem/origin/frontend/controller/LanguageSelectorFilter.java
+++ src/main/java/biz/turnonline/ecosystem/origin/frontend/controller/LanguageSelectorFilter.java
@@ -1,5 +1,7 @@
 package biz.turnonline.ecosystem.origin.frontend.controller;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.MutableHttpResponse;
 import io.micronaut.http.annotation.Filter;
@@ -9,6 +11,8 @@ import io.micronaut.http.filter.ServerFilterChain;
 import io.micronaut.http.netty.cookies.NettyCookie;
 import io.reactivex.Flowable;
 import org.reactivestreams.Publisher;
+
+import java.util.List;
 
 /**
  * Filter is used to check language query parameter and if it is provided change locale accordingly
@@ -26,15 +30,25 @@ public class LanguageSelectorFilter
 
     public static final String DEFAULT_LANGUAGE = "en";
 
+    private static final List<String> SUPPORTED_LANGUAGES = Lists.newArrayList( "en", "sk" );
+
     @Override
     protected Publisher<MutableHttpResponse<?>> doFilterOnce( HttpRequest<?> request, ServerFilterChain chain )
     {
         String language = request.getParameters().get( LANGUAGE_PARAM );
-        Cookie cookie = request.getCookies().findCookie( LANGUAGE_COOKIE ).orElse( new NettyCookie( LANGUAGE_COOKIE, DEFAULT_LANGUAGE ) );
+        Cookie cookie = request.getCookies()
+                .findCookie( LANGUAGE_COOKIE )
+                .orElse( new NettyCookie( LANGUAGE_COOKIE, DEFAULT_LANGUAGE ) );
 
         if ( language != null )
         {
             cookie.value( language );
+        }
+
+        String cookieValue = cookie.getValue();
+        if ( !SUPPORTED_LANGUAGES.contains( Strings.isNullOrEmpty( cookieValue ) ? "" : cookieValue ) )
+        {
+            cookie.value( DEFAULT_LANGUAGE );
         }
 
         return Flowable.fromPublisher( chain.proceed( request ) )
