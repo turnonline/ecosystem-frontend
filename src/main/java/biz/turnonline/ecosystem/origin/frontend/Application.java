ab9681b7c01d2509f0aa0bb00b384b2d25bf0c93
Aurel Medvegy <medvegy@turnonline.biz>
2023-11-11 21:59:13 +0100

    Micronaut migration from 3 -> 4.0.0

--- src/main/java/biz/turnonline/ecosystem/origin/frontend/Application.java
+++ src/main/java/biz/turnonline/ecosystem/origin/frontend/Application.java
@@ -1,5 +1,9 @@
 package biz.turnonline.ecosystem.origin.frontend;
 
+import io.micronaut.context.ApplicationContextBuilder;
+import io.micronaut.context.ApplicationContextConfigurer;
+import io.micronaut.context.annotation.ContextConfigurer;
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.runtime.Micronaut;
 
 public class Application
@@ -7,5 +11,16 @@ public class Application
     public static void main( String[] args )
     {
         Micronaut.run( Application.class );
+    }
+
+    @ContextConfigurer
+    public static class DeduceCloudEnvironmentConfigurer
+            implements ApplicationContextConfigurer
+    {
+        @Override
+        public void configure( @NonNull ApplicationContextBuilder builder )
+        {
+            builder.deduceCloudEnvironment( true );
+        }
     }
 }
